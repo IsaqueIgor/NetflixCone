eb596287a638e2ad958910cd481ae66fd71371b2
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2019-12-02 09:12:06 -0200

    struct files and project

---
+++ netflix-clone/src/pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import TabComponent from '../components/TabComponent';
+import Footer from '../components/Footer';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Main;
